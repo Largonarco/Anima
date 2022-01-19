1eded9bcb9718b5fc30736295afc222e4a9376d4
Largonarco <snarayankars62@gmail.com>
2022-01-19 13:34:50 +0530

    production

--- url.js
+++ url.js
@@ -1,7 +1,7 @@
 let URL;
 
 if (process.env.NODE_ENV === "production") {
-  URL = "https://anima-zeta.vercel.app";
+  URL = "https://anima-woad.vercel.app";
 } else {
   URL = "http://localhost:3000";
 }
